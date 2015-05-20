1b9d455a18a410483f66728f11a67390538f5b02
Patricio Cerda Mardini <pcerda2@uc.cl>
2015-05-20 20:22:16 -0300

    El código ya tiene lista la parte para leer archivos como los del enunciado.

--- DPLL Nuestro.py
+++ DPLL Nuestro.py
@@ -1,3 +1,33 @@
-__author__ = 'Patricio Cerda & Joaquín Moreno & Pedro Zepeda'
+__author__ = 'Patricio Cerda, Joaquin Moreno y Pedro Zepeda'
 
-#Acá irá todo
+def backtracking (s):
+    if es_objetivo(s): return True
+    for h in s.sucesores:
+        if no_ha_sido_explorado(h):
+            if backtracking(h): return True
+    return False
+
+def LeerArchivo(ruta):
+    lista_clausulas = []
+    archivo = open(ruta, 'r')
+    linea   = archivo.readline()
+    while linea != '':
+        pos1   = linea.find(' ')
+        valor1 = int(linea[:pos1])
+        resto  = linea[pos1 + 1:len(linea) - 1]
+        pos2   = resto.find(' ')
+        valor2 = int(resto[:pos2])
+        valor3 = int(resto[pos2 +1:len(resto)])
+        tupla  = (valor1, valor2, valor3)
+        lista_clausulas.append(tupla)
+        linea  = archivo.readline()
+    archivo.close()
+    return lista_clausulas
+
+while True:
+    opcion = input('Seleccione el archivo a probar. Hay 20 archivos. \n')
+
+    r = './instancia%s.txt' % opcion
+    lista_clausulas = LeerArchivo(r)
+
+    print(lista_clausulas)
