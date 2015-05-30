8881667083add4578ab152457ca42dff43a9bf76
Patricio Cerda Mardini <pcerda2@uc.cl>
2015-05-30 00:37:04 -0300

    Cambios

--- Ignore/DPLL P1.py
+++ Ignore/DPLL P1.py
@@ -3,7 +3,7 @@ __author__ = 'Patricio Cerda, Joaquin Moreno y Pedro Zepeda'
 import os
 from time import time
 
-#Pseudocodigo y definiciones de lo que se debe hacer para optimizar el algoritmo base:
+#Pseudocodigo de lo que se debe hacer para optimizar el algoritmo base:
 #
 #Unit propagation
 #If a clause is a unit clause, i.e. it contains only a single unassigned literal,
@@ -37,20 +37,18 @@ from time import time
 #replace every occurrence of l with "true" and every occurrence of not l with "false" in the formula ?, and
 #simplify the resulting formula.
 
-### DPLL BASICO ###
-
 def DPLL(alpha, recorrido):
-    print('\nExpresion actual', alpha)
-    if alpha == '': print('Alpha es nada, por lo tanto True'); print('Para dar True, los valores a tomar para cada literal deben ser:', recorrido); return True
-    elif alpha == []: print('Alpha es vacia, por lo tanto False'); return False
-    elif (len(alpha[0]) == 1):
-        print('Alpha es clausula unitaria, por lo tanto simplificamos.')
-        literal_1 = alpha[0][0] # == True?
+    print('Expresion actual', alpha)
+    if alpha == '': print('alpha es nada, por lo tanto True'); return True
+    elif alpha == []: print('alpha es vacia, por lo tanto False'); return False
+    elif (len(alpha[0]) == 1) and (alpha[0].isalpha()):
+        print('alpha es clausula unitaria, por lo tanto simplificamos.') 
+        literal_1 == alpha[2] # == True?
         print('Debiese asignarse True a ', literal_1)
-        return DPLL(simplificacion(alpha, literal_1, recorrido), recorrido)
+        return DPLL(simplificacion(alpha, literal_1))
     i = 0; j = 0
     literal_2 =  alpha[i][j]
-    while abs(literal_2) in recorrido:
+    while literal_2 in recorrido:
         try: 
             literal_2 = alpha[i][j+1]
         except: 
@@ -59,43 +57,39 @@ def DPLL(alpha, recorrido):
                 literal_2 = alpha[i+1][j]
             except: break
     recorrido.append(literal_2)
-    print('Asigno valor de verdad a', literal_2, 'y modifico el recorrido, que ahora es:', recorrido)
-    if DPLL(simplificacion(alpha, literal_2, recorrido), recorrido) == True:
+    print('Asigno valor de verdad a ', literal_2, 'y modifico el recorrido, que ahora es: ', recorrido)
+    if DPLL(simplificacion(alpha, literal_2), recorrido) == True: 
         return True
     else: 
         print('No resulto la asignacion, probamos con el valor de verdad contrario para el mismo literal.')
-        return DPLL(simplificacion(alpha, -literal_2, recorrido),recorrido)
+        return DPLL(simplificacion(alpha, -literal_2),recorrido)
 
-def simplificacion(alpha,literal, camino):
-    """remueve clausulas en alpha donde el literal es afirmativo (positivo).
-    Ademas, remover (no literal) de clausulas donde aparece.
-    Finalmente, return new alpha"""
+def simplificacion(alpha,literal):
+    """remover clausulas in alpha donde literal es afirmativo/positivo
+    remover (no literal) de clausulas donde aparece
+    return new alpha"""
     i = 1
     largo = len(alpha)
     if len(alpha) == 1: largo = largo + 1
     while i in range(0,largo):
         i -= 1
         clausula = alpha[i]
-        print('Alpha a simplificar es', alpha)
+        print('Alpha que entra es ', alpha)
         j = 0
         while j in range(0,2):
             atomo = clausula[j]
-            print('En la clausula ', clausula, ' se tiene el atomo', atomo)
+            print('En la clausula ', clausula, ' se tiene el atomo ', atomo)
             if atomo == -literal:
-                print('Este atomo es opuesto al literal asignado, asi que lo remuevo.')
+                print('Atomo opuesto, remuevo el atomo.')
                 clausula.remove(atomo)
                 j = 0
-                if len(clausula) == 0: alpha.remove(clausula);break
-                if len(clausula) == 1: j -= 1 #Agregado por error feo.
                 continue
             if atomo == literal:
-                print('Este atomo es igual al literal asignado, asi que remuevo la clausula')
+                print('Atomo igual, remuevo clausula')
                 alpha.remove(clausula)
                 j = 1
                 if len(alpha) >= 2: i += 1
-                if len(alpha) == 0:
-                    if abs(atomo) not in camino and atomo not in camino: camino.append(atomo)
-                    return '' # No confundir, este no va indentado en el if del camino!!
+                if len(alpha) == 0: return ''
                 break
             j += 1
     return alpha
@@ -104,10 +98,10 @@ def LeerArchivo(ruta):
     lista_clausulas = []
     archivo = open(ruta, 'r')
     linea   = archivo.readline()
-    while linea != '' and linea != '\n':
+    while linea != '':
         pos1   = linea.find(' ')
         valor1 = int(linea[:pos1])
-        resto  = linea[pos1 + 1:len(linea)]
+        resto  = linea[pos1 + 1:len(linea) - 1]
         pos2   = resto.find(' ')
         valor2 = int(resto[:pos2])
         valor3 = int(resto[pos2 +1:len(resto)])
@@ -126,8 +120,11 @@ while True:
         break
 
     try:
-        r = '%s/instancia%s.txt' % (path,opcion)
+        r = '%s/instancia%s.txt' % path,opcion
+        print('r', r)
         lista_clausulas = LeerArchivo(r)
+
+        print(lista_clausulas)
 
     except:
         print("Archivo instancia%s.txt no encontrado. Intente de nuevo!" % opcion)
